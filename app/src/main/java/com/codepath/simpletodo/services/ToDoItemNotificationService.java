02b790c59de116151d372e50b63fc6821654ac6f
sharathp <chandra.ps@gmail.com>
2016-06-22 22:22:44 -0400

    Fixed notification. Fixed alarm to be fired at the right time and no duplicates.

--- app/src/main/java/com/codepath/simpletodo/services/ToDoItemNotificationService.java
+++ app/src/main/java/com/codepath/simpletodo/services/ToDoItemNotificationService.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.StringRes;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.WakefulBroadcastReceiver;
 import android.util.Log;
@@ -82,26 +83,30 @@ public class ToDoItemNotificationService extends IntentService {
         final NotificationManager mNotificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        final StringBuilder sb = new StringBuilder();
-        sb.append("Total ToDo Items for today: ")
-                .append(high + medium + low)
-                .append(" [High: ").append(high)
-                .append("; Medium: ").append(high)
-                .append("; Low: ").append(high)
-                .append("]");
-
         final int requestID = (int) System.currentTimeMillis();
         final Intent intent = MainActivity.createIntent(this);
         final PendingIntent pIntent = PendingIntent.getActivity(this, requestID, intent, 0);
 
+        final NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+        inboxStyle.setBigContentTitle(String.format(getString(R.string.notif_big_content_title), (high + medium + low)));
+        addNotificationEntry(inboxStyle, R.string.notif_content_high, high);
+        addNotificationEntry(inboxStyle, R.string.notif_content_medium, medium);
+        addNotificationEntry(inboxStyle, R.string.notif_content_low, low);
+
         final NotificationCompat.Builder builder =
                 new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.mipmap.ic_launcher)
-                        .setContentTitle("SimpleToDo Reminder")
-                        .setContentText(sb.toString())
+                        .setSmallIcon(R.drawable.ic_alarm_black)
+                        .setContentTitle(getString(R.string.notif_title))
+                        .setStyle(inboxStyle)
                         .setContentIntent(pIntent)
                         .setAutoCancel(true);
 
         mNotificationManager.notify(0, builder.build());
+    }
+
+    private void addNotificationEntry(final NotificationCompat.InboxStyle inboxStyle, @StringRes int priorityStringId, int count) {
+        if (count > 0) {
+            inboxStyle.addLine(String.format(getString(priorityStringId), count));
+        }
     }
 }
